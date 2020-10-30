61a701de23ca006c0fefe7498c169171acc3bbf5
Simone <simone.sacchi@nubilaria.com>
2020-10-31 00:12:57 +0100

    firs build output

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,51 @@
+"use strict";
+
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-    mode: "development",
+
+module.exports = function (env) {
+    
+    const IS_PROD = (env && env.production) ? true : false;
+    const MODE = IS_PROD ? "production" : "development";
+
+    var optimization = {};
+    if (IS_PROD) {
+        optimization.minimize = true;
+    }
+    
+    return {
+        mode: MODE,
+        output: {
+            path: path.resolve(__dirname, 'public'),
+        },
+        devtool: false,
+        optimization: optimization,
         devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9000
-    },
+            contentBase: path.resolve(__dirname, 'public'),
+            compress: true,
+            port: 9001
+        },
 
-    module: {
-        /** 
-        rules: [
-            {
-                test: /\.html$/i,
-                use: 'raw-loader',
-            },
-        ],
-        */
-    },
+        module: {
+            /** 
+            rules: [
+                {
+                    test: /\.html$/i,
+                    use: 'raw-loader',
+                },
+            ],
+            */
+        },
 
 
-    resolve: {
-        modules: ['node_modules', __dirname]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin()
-    ]
+        resolve: {
+            modules: ['node_modules', __dirname]
+        },
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin()
+        ]
+    }
 };
