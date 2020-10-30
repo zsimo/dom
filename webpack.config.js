f414f0db8f7841b250a9d65c8871173f65401403
Simone <simone.sacchi@nubilaria.com>
2020-10-30 23:49:12 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+        devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    },
+
+    module: {
+        /** 
+        rules: [
+            {
+                test: /\.html$/i,
+                use: 'raw-loader',
+            },
+        ],
+        */
+    },
+
+
+    resolve: {
+        modules: ['node_modules', __dirname]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin()
+    ]
+};
