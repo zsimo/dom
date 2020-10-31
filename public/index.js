88224e95f7b354b0b7b104a18034de2aed82548a
Simone <simone.sacchi@nubilaria.com>
2020-10-31 23:42:19 +0100

    working on index

--- public/index.js
+++ public/index.js
@@ -1,1 +1,1 @@
-(()=>{var e=document.getElementById("container");document.getElementById("child"),e.addEventListener("click",(function(e){var o=e.target;console.log("clientHeight",o.clientHeight),console.log("scrollHeight",o.scrollHeight),console.log("scrollTop",o.scrollTop),console.log("offsetHeight",o.offsetHeight),console.log("clientTop",o.clientTop)}))})();
+console.log("csiao");
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,39 +1,4 @@
 
-var container = document.getElementById("container");
-var child = document.getElementById("child");
-container.addEventListener("click", click);
 
-function click (event) {
-    var target = event.target;
 
-    console.log("clientHeight", target.clientHeight); //  CSS height + CSS padding - height of horizontal scrollbar (if present)
-    console.log("scrollHeight", target.scrollHeight); // value is equal to the minimum height the element would require in order to fit all the content in the viewport without using a vertical scrollbar. The height is measured in the same way as clientHeight
-    console.log("scrollTop", target.scrollTop); //  gets or sets the number of pixels that an element's content is scrolled vertically.
-
-    // scroll at the end
-    // element.scrollHeight - element.scrollTop === element.clientHeight
-
-    console.log("offsetHeight", target.offsetHeight); //  CSS height + CSS padding CSS border + height of horizontal scrollbar (if present)
-     
-    console.log("clientTop", target.clientTop);
-
-}
-
-function check () {
-    console.log("container.clientHeight",container.clientHeight, "container.scrollHeight",container.scrollHeight);
-    console.log("child.offsetTop",child.offsetTop, "container.scrollTop", container.scrollTop);
-
-    // down
-    if (child.offsetTop + child.clientHeight > container.clientHeight + container.scrollTop) {
-        console.log("go down")
-        container.scrollTop += child.clientHeight;
-    }
-    else if (child.offsetTop < container.scrollTop) {
-        console.log("go up")
-        container.scrollTop -= child.clientHeight;
-    }
-    // up
-    else {
-        console.log("?")
-    }
-}
+console.log("csiao")
