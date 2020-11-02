f305c183133cf9c40439f6378dbf220cbe3dd47f
Simone <simone.sacchi@nubilaria.com>
2020-11-02 09:45:06 +0100

    first index page

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,2 +1,47 @@
 
 
+
+function play () {
+
+    var header = document.querySelector("header");
+    var main = document.querySelector("main");
+
+    var col = main.querySelectorAll(".col")[1];
+    var newCol = document.createElement("div");
+    newCol.className = "col";
+
+
+
+    // header
+    var p = document.createElement("p");
+    p.innerText = "header heigth " + header.clientHeight;
+    newCol.appendChild(p);
+    // main
+    p = document.createElement("p");
+    p.innerText = "main clientHeight " + main.clientHeight +
+                    ", offsetTop " + main.offsetTop +
+                    ", scrollHeight " + main.scrollHeight +                
+                    ", offsetParent " + main.offsetParent.tagName;
+    newCol.appendChild(p);
+    p = document.createElement("p");
+    p.innerText = "body heigth " + document.body.clientHeight;
+    newCol.appendChild(p);
+
+    var button = document.createElement("button");
+    button.innerText = "Adhust main height [body.clientHeight - header.clientHeight]";
+    button.onclick = function () {
+        main.style.height = document.body.clientHeight - header.clientHeight + "px";
+    };
+    newCol.appendChild(button);
+
+    main.replaceChild(newCol, col);
+
+
+}
+
+
+
+
+window.addEventListener('resize', play);
+
+play();
