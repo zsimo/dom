6bd1e8eb7ec7d5d0309411a4d7399d134090fad3
Simone <simone.sacchi@nubilaria.com>
2020-11-08 09:49:56 +0100

    fix baseurl

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,9 +1,8 @@
 "use strict";
 
-require("/src/common.js");
-
 var main = document.querySelector("main");
 
+console.log(main)
 var pages = [
     "01",
     "02",
@@ -14,7 +13,7 @@ var ul = document.createElement("ul");
 pages.forEach(function (page) {
     var li = document.createElement("li");
     var a = document.createElement("a");
-    a.href = "/" + page;
+    a.href = window.location.href + page;
     a.innerText = "page #" + page;
     li.appendChild(a);
     ul.appendChild(li);
